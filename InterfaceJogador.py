bc3ea50781cb8863f2192f1476dbb4d9629efe35
Bruno Vazquez <brunovazquez@Brunos-MacBook-Air.local>
2025-05-11 12:22:16 -0300

    entrega 2

--- InterfaceJogador.py
+++ InterfaceJogador.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import FALSE, Menu, messagebox
 from tkinter.messagebox import showinfo
 from Tabuleiro import Tabuleiro
 from dog.dog_actor import DogActor
@@ -9,18 +9,41 @@ class InterfaceJogador(DogPlayerInterface):
     
     def __init__(self):
         self.tk = tk.Tk()
+        self.menubar = Menu(self.tk)
+        self.arquivo = Menu(self.menubar, tearoff=0)
+        self.arquivo.add_command(label='Iniciar jogo', command=self.iniciar_partida)
+        self.arquivo.add_command(label='Restaurar estado inicial', command=self.start_game)
+        self.menubar.add_cascade(label='Arquivo', menu=self.arquivo)
+        self.tk.config(menu=self.menubar) 
         self.tabuleiro = Tabuleiro(self.tk)
         self.dog_server_interface = DogActor()
-        message = self.dog_server_interface.initialize("Tiago", self)
+        message = self.dog_server_interface.initialize(self.tabuleiro.player_name, self)
         messagebox.showinfo(message=message)
     
     def iniciar_partida(self):
-        start_status = self.__dog_server_interface.start_match(2)
+        status_partida = self.tabuleiro.get_status_partida()
+        if status_partida == 1:
+            resposta = messagebox.askyesno("START", "Deseja iniciar uma nova partida?")
+            if resposta:
+                start_status = self.dog_server_interface.start_match(2)
+                codigo = start_status.get_code()
+                menssagem = start_status.get_message()
+                if codigo == "0" or codigo == "1":
+                    messagebox.showinfo(message=menssagem)
+                else:  #    (code=='2')
+                    jogadores = start_status.get_players()
+                    jogadore_local_id = start_status.get_local_id()
+                    self.tabuleiro.iniciar_partida(jogadores, jogadore_local_id)
+                    game_state = self.tabuleiro.get_status()
+                    self.update_gui(game_state)
+                    messagebox.showinfo(message=start_status.get_message())
+    
+    def receber_inicio(self, start_status):
         message = start_status.get_message()
         messagebox.showinfo(message=message)
-    
-    def receber_inicio(self):
-        self.__show_popup_message("Recebe início")
+
+    def start_game(self):
+        print("Iniciando jogo...")
 
     def receive_start(self) -> None:
         pass  
--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,24 +1,42 @@
 from tkinter import Canvas
+from tkinter import simpledialog
 from Peca import Peca
+import Jogador as jogador
+
+# 	Board matchStatus
+# 1 - no match (initial state)
+# 2 - finished match (game with winner - no tied game)
+# 3 - your turn, match in progress AND NOT move occurring (waiting first action)
+# 4 - your turn, match in progress AND move occurring (waiting second action)
+# 5 - NOT your turn, match in progress - waiting move
+# 6 - match abandoned by opponent
 
 
 class Tabuleiro:
     def __init__(self, root, grid_size=5):
+        self.jogador_local = jogador.Jogador()
+        self.jogador_remoto = jogador.Jogador()
+        self.jogador_local.inicializar(1, "Red player", "Red player")
+        self.jogador_remoto.inicializar(2, "Blue player", "Blue player")
+        self.status_partida = 1
         self.grid_size = grid_size
         self.board_margin = 50
         self.canvas_width = 500
         self.canvas_height = 500
         self.cell_size = (self.canvas_width - 2 * self.board_margin) / self.grid_size
         self.pieces = []
+        self.status_partida = 1
 
         self.canvas = Canvas(root, width=self.canvas_width, height=self.canvas_height, bg="#333", highlightthickness=0)
         self.canvas.pack(padx=20, pady=20)
 
-        self.draw_board()
+        self.draw_board() # preciso colocar isso no diagrama de sequencia como uma self message de Tabuleiro???
         self.canvas.bind("<Button-1>", self.on_click)
 
         self.current_symbol = "X"
         self.current_color = "#cc0000"
+
+        self.player_name = simpledialog.askstring("Nome do Jogador", "Digite seu nome:")
 
     def draw_board(self):
         board_size = self.canvas_width - (2 * self.board_margin)
@@ -39,6 +57,23 @@ class Tabuleiro:
                 self.board_margin + i * self.cell_size, self.board_margin + board_size,
                 fill="white", width=1
             )
+
+    def iniciar_partida(self, jogadores, local_id):
+        playerA_name = jogadores[0][0]
+        playerA_id = jogadores[0][1]
+        playerA_order = jogadores[0][2]
+        playerB_name = jogadores[1][0]
+        playerB_id = jogadores[1][1]
+        self.jogador_local.reset()
+        self.jogador_remoto.reset()
+        self.jogador_local.inicializar(1, playerA_id, playerA_name)
+        self.jogador_remoto.inicializar(2, playerB_id, playerB_name)
+        if playerA_order == "1":
+            self.jogador_local.alternar_turno()
+            self.status_partida = 3  #    waiting piece or origin selection (first action)
+        else:
+            self.jogador_remoto.alternar_turno()
+            self.status_partida = 5  #    waiting remote action
 
     def add_piece(self, peca: Peca):
         self.pieces.append(peca)
@@ -74,4 +109,7 @@ class Tabuleiro:
         else:
             self.current_symbol = "X"
             self.current_color = "#cc0000"
+
+    def get_status_partida(self):
+        return self.status_partida
 
